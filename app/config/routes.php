f84b8266706277c42b212b95a46958b555a07a44
Kevin <kevinbalita80@gmail.com>
2026-09-09 23:46:31 +0800

    Show login screen at application root

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,7 +3,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 /** @var object $router **/
 
-$router->get('/', 'Welcome::index');
+$router->get('/', 'AuthController::login');
 $router->get('/student', 'StudentController::index');
 $router->get('/student/profile', 'StudentController::profile')->middleware('student_access');
 $router->get('/student/grant_access', 'StudentController::grant_access');
